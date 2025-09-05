f19e54d9bab8c588acdc4160738b1d52c828a2d9
Maximiliano Principe <mlapeducacionit@gmail.com>
2025-09-05 14:43:13 -0300

    Empezando con sombras

---
+++ 01.box-shadow-sombras/src/constants/menu-items.js
@@ -0,0 +1,8 @@
+const menuItems = [
+    { id: 1, nombre: 'Productos', ruta: '/productos' },
+    { id: 2, nombre: 'Productos', ruta: '/productos' },
+    { id: 3, nombre: 'Productos', ruta: '/productos' },
+    { id: 1, nombre: 'Productos', ruta: '/productos' },
+]
+
+export default menuItems
